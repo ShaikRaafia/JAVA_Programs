ecba3b3b9e6596b8685e8adde18c5766a9a2831b
ShaikRaafia <raafia2346@gmail.com>
2021-12-28 12:21:30 +0530

    committed

---
+++ DAY-8/Compression.java
@@ -0,0 +1,26 @@
+public class Compression {
+    public static void main(String[] args) {
+        String str = "hhhyqqpppp";
+        for(int i=0; i<str.length();i++)
+        {
+            int count=0;
+            char ch=str.charAt(i);
+            if(ch!='\0')
+            {
+            for(int j=i; j<str.length(); j++)
+            {
+                if(ch==str.charAt(j))
+                    count++;
+                else
+                {
+                    break;
+                }
+            }
+            System.out.print(ch+""+count);
+        }
+            str=str.replace(ch,'\0');
+        }
+            
+    }
+    
+}
